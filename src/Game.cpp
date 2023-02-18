d179afd9a7ccd07a06473b6f7cb94f05204b72b2
Adriano Valadar <60299267+adrianovaladar@users.noreply.github.com>
2023-02-18 17:12:21 +0000

    Update Game and add main

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,5 +1,26 @@
-//
-// Created by rogerio on 2/18/23.
-//
-
 #include "Game.h"
+
+int Game::getTurnsAvailable() const {
+    return turnsAvailable;
+}
+void Game::setTurnsAvailable(int turnsAvailable) {
+    Game::turnsAvailable = turnsAvailable;
+}
+int Game::getTurnsRecover() const {
+    return turnsRecover;
+}
+void Game::setTurnsRecover(int turnsRecover) {
+    Game::turnsRecover = turnsRecover;
+}
+int Game::getNumDemons() const {
+    return numDemons;
+}
+void Game::setNumDemons(int numDemons) {
+    Game::numDemons = numDemons;
+}
+int Game::getNumTurns() const {
+    return numTurns;
+}
+void Game::setNumTurns(int numTurns) {
+    Game::numTurns = numTurns;
+}
--- src/Game.h
+++ src/Game.h
@@ -1,12 +1,24 @@
 #ifndef REPLY_2022_GAME_H
 #define REPLY_2022_GAME_H
 
+#include "Character.h"
 
 class Game {
     int turnsAvailable;
     int turnsRecover;
     int numDemons;
     int numTurns;
+    Character pandora;
+
+public:
+    int getTurnsAvailable() const;
+    void setTurnsAvailable(int turnsAvailable);
+    int getTurnsRecover() const;
+    void setTurnsRecover(int turnsRecover);
+    int getNumDemons() const;
+    void setNumDemons(int numDemons);
+    int getNumTurns() const;
+    void setNumTurns(int numTurns);
 };
 
 
